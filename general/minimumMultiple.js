97506209dc139dd4a08f2eb8bc94043f10f5f04e
vidaaudrey <audrey@33road.com>
2016-03-01 12:40:53 -0800

    add minumMultiple (not in detail)

---
+++ general/minimumMultiple.js
@@ -0,0 +1,7 @@
+function minimulMultiple(divisor, lowerBound) {
+    var i  = Math.ceil(lowerBound / divisor)
+    // console.log(i * divisor)
+    return i * d
+}
+minimulMultiple(5, 12)
+// expect to output 15 
